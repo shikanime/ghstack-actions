370df03e38ff07127f96c8a9e658cc30238cfb7a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-25 02:00:37 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<!-- markdownlint-disable first-line-heading -->
+
+![header.png](https://raw.githubusercontent.com/shikanime/shikanime/main/assets/github-header.png)
+
+<!-- markdownlint-enable first-line-heading -->
+
+## About me
+
+Hey 🌸 I'm Shikanime Deva, this is my software automation platform.
---
+++ nix/devenv.nix
@@ -0,0 +1,43 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        enableDefaultExcludes = true;
+        programs = {
+          actionlint.enable = true;
+          deadnix.enable = true;
+          nixfmt.enable = true;
+          prettier.enable = true;
+          shfmt.enable = true;
+          statix.enable = true;
+          terraform.enable = true;
+        };
+        settings.global.excludes = [
+          ".devenv/*"
+          ".direnv/*"
+          ".sl/*"
+          "LICENSE"
+        ];
+      };
+      devenv.shells.default = {
+        pre-commit.hooks.flake-checker.enable = true;
+        containers = pkgs.lib.mkForce { };
+        languages = {
+          nix.enable = true;
+          terraform = {
+            enable = true;
+            package = pkgs.opentofu;
+          };
+        };
+        cachix = {
+          enable = true;
+          push = "shikanime";
+        };
+        packages = [
+          pkgs.gh
+        ];
+      };
+    };
+}
